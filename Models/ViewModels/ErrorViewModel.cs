54030e7f8053a21d2783e671b790611d90a8a6ab
RodolfoBarroso <rodolfocesar24@gmail.com>
2020-02-13 23:16:41 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VendasWebMvc.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
