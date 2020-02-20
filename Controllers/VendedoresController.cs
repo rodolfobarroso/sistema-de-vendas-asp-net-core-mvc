f8b1809d6e7e2ad7de4495d11530b1e10c38f751
RodolfoBarroso <rodolfocesar24@gmail.com>
2020-02-19 23:11:37 -0300

    Adicionando formulário simples de cadastro - Vendedor

--- Controllers/VendedoresController.cs
+++ Controllers/VendedoresController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using VendasWebMvc.Services;
+using VendasWebMvc.Models;
 
 namespace VendasWebMvc.Controllers
 {
@@ -19,6 +20,19 @@ namespace VendasWebMvc.Controllers
         {
             var list = _vendedorService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Vendedor vendedor)
+        {
+            _vendedorService.Insert(vendedor);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
