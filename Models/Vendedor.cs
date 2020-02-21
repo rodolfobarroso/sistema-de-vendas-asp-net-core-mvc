af6c73cf07ad74e266a43137b1f4ab53dcb9e78d
RodolfoBarroso <rodolfocesar24@gmail.com>
2020-02-21 18:09:24 -0300

    Localização, formatação de numeros e datas

--- Models/Vendedor.cs
+++ Models/Vendedor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace VendasWebMvc.Models
@@ -8,8 +9,17 @@ namespace VendasWebMvc.Models
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        
+        [Display(Name = "Data de Nascimento")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime DataNascimento { get; set; }
+
+        [Display(Name = "Salário Base")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double SalarioBase { get; set; }
         public Departamento Departamento { get; set; }
         public int DepartamentoId { get; set; } //garantir a integridade referencial - foreignKey
